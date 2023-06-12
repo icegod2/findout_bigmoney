0d5a38a2134823bca9ef701cece62acac12dd92f
icegod <icegod0803@gmail.com>
2023-06-13 00:31:47 +0800

    update_stock_data support start from specified one stock id for saving time

--- stock.py
+++ stock.py
@@ -6,6 +6,7 @@ from FinMind.data import DataLoader
 from FinMind import plotting
 from random import randint
 from time import sleep
+from datetime import date
 
 
 stocklist_fn = "stocklist.csv"
@@ -25,13 +26,14 @@ def update_stock_list():
     return df
 
 
-def update_stock_data():
+def update_stock_data(start=1):
     stocklist = pd.read_csv(stocklist_fn)
     for index, row in stocklist.iterrows():
         curr_stock_id = int(row['有價證券代號'])
         curr_stock_name = row['有價證券名稱']
-        get_stock_info(curr_stock_id, curr_stock_name)
-        sleep(randint(10, 30))
+        if curr_stock_id >= start:
+            get_stock_info(curr_stock_id, curr_stock_name)
+            sleep(randint(10, 30))
 
 
 def get_stock_info(stock_no, stock_name):
@@ -40,9 +42,9 @@ def get_stock_info(stock_no, stock_name):
         print("Directory '% s' created" % save_data_folder)
 
     dl = DataLoader()
+    today = date.today()
     stock_data = dl.taiwan_stock_daily(
-        stock_id=stock_no, start_date='2000-01-01', end_date='2022-12-31')
-    plotting.kline(stock_data)
+        stock_id=stock_no, start_date='2000-01-01', end_date=today)
     fn = data_fn_fmt.format(save_data_folder, stock_no, stock_name)
     print("save to {}".format(fn))
     if os.path.exists(fn):
@@ -52,12 +54,10 @@ def get_stock_info(stock_no, stock_name):
 
 
 def show_stock_info(stock_no, stock_name):
-    print("show_stock_info get {} {}".format(stock_no, stock_name))
     fn = data_fn_fmt.format(save_data_folder, stock_no, stock_name)
     plot_fn = "{}.html".format(fn)
-    print("fn => {}".format(fn))
-    print("plot_fn => {}".format(plot_fn))
     stocklist = pd.read_csv(fn)
+    print(stocklist)
     plotting.kline(stocklist, filename=plot_fn)
 
 
@@ -65,7 +65,7 @@ def test_callback(stock_no):
     stocklist = pd.read_csv(stocklist_fn)
     print("test_callback get stock_no:{}".format(stock_no))
 
-    stock = stocklist[(stocklist['有價證券代號'] == 2330)].head(1)
+    stock = stocklist[(stocklist['有價證券代號'] == stock_no)].head(1)
     # stock = stocklist.query("有價證券代號 == 2330")
 
     if len(stock) > 0:
@@ -76,21 +76,18 @@ def test_callback(stock_no):
     else:
         print('無此股票代號')
 
-    # for index, row in stocklist.head(10).iterrows():
-    #     break
-    #     curr_stock_id = int(row['有價證券代號'])
-    #     curr_stock_name = row['有價證券名稱']
-    #     if curr_stock_id == stock_no:
-    #         show_stock_info(curr_stock_id, curr_stock_name)
-    #         break
 
 
 def main():
-    parser = optparse.OptionParser(usage="%prog [-u]", version="%prog 1.0")
+    parser = optparse.OptionParser(usage="%prog [-u] [-t]", version="%prog 1.0")
     parser.add_option('-u', dest='type',
                       type='string',
                       help='update [ list| data]')
 
+    parser.add_option('-s', dest='num',
+                      type='int',
+                      help='update [list| data] -s num')
+    
     parser.add_option('-t', dest='id',
                       type='int',
                       help='test stock_id')
@@ -101,12 +98,14 @@ def main():
         if options.type == "list":
             update_stock_list()
         elif options.type == "data":
-            update_stock_data()
+            if options.num != None:
+                update_stock_data(options.num)
+            else:
+                update_stock_data()
         else:
             print(parser.usage)
-            exit(0)
+            exit(1)
 
-    print(options.id)
     if options.id != None:
         test_callback(options.id)
 
